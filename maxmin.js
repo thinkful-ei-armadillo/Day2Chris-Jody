abc75125b83dabe0b5d663dfc83e7e8d9ce200e5
Jodster1010 <jody.cook@mac.com>
2019-01-15 09:20:36 -0800

    commit 2

--- maxmin.js
+++ maxmin.js
@@ -1,12 +1,24 @@
 'use strict';
 function max(numbers) {
-    let nowMax = numbers[0];
-    let i = 0;
-    while (i < numbers.length) {
-        if (numbers[i] > nowMax) {
-            nowMax = numbers[i];
-        }
-        i++;
+  let nowMax = numbers[0];
+  let i = 0;
+  while (i < numbers.length) {
+    if (numbers[i] > nowMax) {
+      nowMax = numbers[i];    
     }
-    return nowMax;
+    i++;   
+  }
+  return nowMax;
+}
+
+function min(numbers) {
+  let nowMin = numbers[0];
+  let i = 0;
+  while (i < numbers.length) {
+    if (numbers[i] < nowMin) {
+      nowMin = numbers[i];    
+    }
+    i++;   
+  }
+  return nowMin;
 }
