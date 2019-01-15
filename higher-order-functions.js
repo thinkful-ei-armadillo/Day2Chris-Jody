8a601278f2c95b1c7cb52519d8abc04c09a6d68f
Jodster1010 <jody.cook@mac.com>
2019-01-15 13:02:00 -0800

    commit 6

--- higher-order-functions.js
+++ higher-order-functions.js
@@ -15,30 +15,46 @@
 // repeat(hello, 5);
 // repeat(goodbye, 5);
 
-function filter(arr, fn) {
-  // TASK: Define your function here
-  const newArray = [];
-  for (let i = 0; i < arr.length; i++){
-    if (fn(arr[i]) === true){
-      newArray.push(arr[i]);
-    }
-  }
-  return newArray; 
-}
+// function filter(arr, fn) {
+//   // TASK: Define your function here
+//   const newArray = [];
+//   for (let i = 0; i < arr.length; i++){
+//     if (fn(arr[i]) === true){
+//       newArray.push(arr[i]);
+//     }
+//   }
+//   return newArray; 
+// }
 
-// DO NOT EDIT BETWEEN THESE LINES, BUT DO READ THE CODE ----->
-// First we setup an array of strings we plan to filter:
-const myNames = ['Rich', 'Joe', 'Bhaumik', 'Ray'];
+// // DO NOT EDIT BETWEEN THESE LINES, BUT DO READ THE CODE ----->
+// // First we setup an array of strings we plan to filter:
+// const myNames = ['Rich', 'Joe', 'Bhaumik', 'Ray'];
 
-// We use your `filter` function here, capturing a new array into `filteredNames`
-// comprised of names that only begin with 'R'
-const filteredNames = filter(myNames, function(name) {
-  // This is known as a "predicate function" - it's a function that 
-  // only returns a boolean
-  return name[0] === 'R';
-});
+// // We use your `filter` function here, capturing a new array into `filteredNames`
+// // comprised of names that only begin with 'R'
+// const filteredNames = filter(myNames, function(name) {
+//   // This is known as a "predicate function" - it's a function that 
+//   // only returns a boolean
+//   return name[0] === 'R';
+// });
 
-console.log(filteredNames); // => ['Rich', 'Ray']
-// <---- DO NOT EDIT BETWEEN THESE LINES
+// console.log(filteredNames); // => ['Rich', 'Ray']
+// // <---- DO NOT EDIT BETWEEN THESE LINES
 
  
+function hazardWarningCreator(typeOfWarning) {
+  let warningCounter = 0;
+  return function(location) {
+    warningCounter ++;
+    console.log(`DANGER! There is a ${typeOfWarning} hazard at ${location}`);
+    console.log(`The ${typeOfWarning} hazard alert has triggered ${warningCounter} time(s) today!`);
+  };
+}
+const rocksWarning = hazardWarningCreator('Rocks on the Road');
+const hiChris = hazardWarningCreator('Chris is on the road');
+const byeChris = hazardWarningCreator('Chris is sleeping');
+
+rocksWarning('Main St and Pacific Ave');
+rocksWarning('Centinela Ave and Olympic Blvd');
+hiChris('Hey, how are you?');
+byeChris('Goodnight');
