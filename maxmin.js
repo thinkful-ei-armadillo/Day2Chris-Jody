de3720abbcfa07942109274331c5acec6c5cd1e5
Christopher <christopher.carnivale@gmail.com>
2019-01-15 14:23:51 -0500

    third commit

--- maxmin.js
+++ maxmin.js
@@ -1,24 +1,34 @@
 'use strict';
-function max(numbers) {
-  let nowMax = numbers[0];
-  let i = 0;
-  while (i < numbers.length) {
-    if (numbers[i] > nowMax) {
-      nowMax = numbers[i];    
-    }
-    i++;   
-  }
-  return nowMax;
+// function max(numbers) {
+//   let nowMax = numbers[0];
+//   let i = 0;
+//   while (i < numbers.length) {
+//     if (numbers[i] > nowMax) {
+//       nowMax = numbers[i];    
+//     }
+//     i++;   
+//   }
+//   return nowMax;
+// }
+
+// function min(numbers) {
+//   let nowMin = numbers[0];
+//   let i = 0;
+//   while (i < numbers.length) {
+//     if (numbers[i] < nowMin) {
+//       nowMin = numbers[i];    
+//     }
+//     i++;   
+//   }
+//   return nowMin;
+// }
+debugger; 
+function average (numbers){
+  let total = 0;
+  numbers.forEach(function(number){
+    total += number; 
+  });
+  return total/numbers.length; 
 }
 
-function min(numbers) {
-  let nowMin = numbers[0];
-  let i = 0;
-  while (i < numbers.length) {
-    if (numbers[i] < nowMin) {
-      nowMin = numbers[i];    
-    }
-    i++;   
-  }
-  return nowMin;
-}
+average ([4, 8]);
